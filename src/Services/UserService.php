7be4c3aac317ef9302e962aa4261ec4b2027b950
Manel <manel.aguilera91@gmail.com>
2023-05-31 23:03:51 +0200

    create a service to common methods

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -1,2 +1,27 @@
 <?php
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\UserNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class UserService 
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+
+    }
+    public function checkUser(string $email): User
+    {
+       
+        if (null === $user = $this->userRepository->findOneBy(['email' => $email])) {
+
+            throw new NotFoundHttpException('La dirección email no existe');
+        }
+
+        return $user;
+    }
+}
